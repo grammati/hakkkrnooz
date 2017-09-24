710a8abf02e884240e3a4bd24d6d2d649847c9b4
Perkins <chperki@amazon.com>
2017-09-24 08:55:12 -0600

    Update deps

--- project.clj
+++ project.clj
@@ -4,19 +4,19 @@
   :min-lein-version "2.0.0"
   :uberjar-name "hakkkrnooz-standalone.jar"
 
-  :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
-                 [org.jsoup/jsoup "1.10.2"]
+  :dependencies [[org.clojure/clojure "1.9.0-beta1"]
+                 [org.jsoup/jsoup "1.10.3"]
 
                  [compojure "1.6.0"]
                  [hiccup "1.0.5"]
 
-                 [cheshire "5.7.1"]
+                 [cheshire "5.8.0"]
 
                  [prone "1.1.4"]
-                 [clj-http "3.5.0"]
-                 [ring/ring-core "1.6.1"]
-                 [ring/ring-jetty-adapter "1.6.1"]
-                 [ring "1.6.1"]]
+                 [clj-http "3.7.0"]
+                 [ring/ring-core "1.6.2"]
+                 [ring/ring-jetty-adapter "1.6.2"]
+                 [ring "1.6.2"]]
 
   :profiles {:dev {:jvm-opts ["-DCACHE_TTL=3000"]}
              :source-paths ["dev"]}
